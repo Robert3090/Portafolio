6ddb9d8d4f29c0e1e1c634622880648c564c6258
Roberto <b-nser@hotmail.com>
2022-03-21 19:03:22 -0600

    Lista la version para celulares

--- scripts.js
+++ scripts.js
@@ -10,17 +10,22 @@ let btnmenu = document.querySelector(".btn-menu"); //este es el boton de hamburg
 let menu = document.querySelector(".navbar"); //este es el menu
 let mainBody = document.querySelector("main"); //este el el contenido de la web
 let link = document.getElementsByTagName("li"); //obtenemos los elementos de la lista
+let body = document.querySelector("html");
 
+if(ancho < 576){
+    menu.classList.add("navbar-hidden");
+}
 
 for(let i = 0; i<link.length;i++){
     link[i].addEventListener('click', (e) => {  
         menu.classList.toggle("navbar-hidden");
-        mainBody.classList.toggle("desplazamiento");
+        body.classList.remove("noScroll");
     });
-  }
+}
 
 
 btnmenu.onclick = () => {
     menu.classList.toggle("navbar-hidden");
     mainBody.classList.toggle("desplazamiento");
-};
+    body.classList.toggle("noScroll");
+}
